dfb32f477d28c4796773543ea7a762374a589189
Alex <AlexSaunders@localhost.local>
2017-05-14 21:09:07 +0100

    ga

--- src/server.js
+++ src/server.js
@@ -128,6 +128,16 @@ class Server {
           <script>
             window.__PRELOADED_STATE__ = ${JSON.stringify(state).replace(/</g, '\\u003c')}
           </script>
+          <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-91502214-2', 'auto');
+            ga('send', 'pageview');
+
+          </script>
           <script src="/static/app.js" async></script>
         </body>
       </html>
