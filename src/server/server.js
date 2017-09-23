0ef67a0cff6575280b8c8d59886b303ba344a48b
Alex Saunders <alexjrsaunders@gmail.com>
2017-09-23 22:45:51 +0100

    Correcting server port

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,10 @@
-import path from 'path';
-import express from 'express';
+import path     from 'path';
+import express  from 'express';
 
-import apiRouter from './api';
-import routes from './routes';
+import apiRouter  from './api';
+import routes     from './routes';
 
-const port = process.env.port || 5000;
+const port = (process.env.PORT || 5000);
 
 const app = express();
 
